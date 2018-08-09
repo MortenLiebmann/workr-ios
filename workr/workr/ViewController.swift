ee082fbc7a37d0a976c0969fa5ca04e195f44b4c
MortenLiebmann <morten_liebmann@hotmail.com>
2018-08-09 11:34:07 +0200

    First commit

---
+++ workr/workr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workr
+//
+//  Created by Morten Liebmann Andersen on 09/08/2018.
+//  Copyright © 2018 Morten Liebmann Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
